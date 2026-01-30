5258ed44ec035b66eeb50781fae7d05e5ad79ec8
Rohit <rohitmandavkar3577@gmail.com>
2026-01-30 13:06:23 +0530

    feat: add new blog post about Cloudflare Tunnel for enhanced resource security

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -11,8 +11,14 @@ const BlogPosts = [
         excerpt: "In meme lang: The ultimate boss of distro fight. A deep dive into why Nix is changing the game.",
         tags: ["Nix", "NixOS", "Linux"],
     },
+    {
+        slug: "cloudflared-tunnel",
+        title: "Cloudflare Tunnel: A Secure Way to Connect Your Resources to Cloudflare",
+        date: "2026-01-30",
+        excerpt: "A deep dive into how Cloudflare Tunnel works and how you can use it to secure your resources.",
+        tags: ["Cloudflare", "Tunnel", "NixOS"],
+    },
 ];
-
 export default function Blogs() {
     return (
         <div className="mx-auto max-w-5xl w-full px-4 md:px-0 font-inter mt-8 md:mt-12 pb-12">
